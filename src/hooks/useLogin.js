7bb46314b10f3d7df02469da60c1d93d6b5f5a14
Connor Smith <134646469+nthconnor@users.noreply.github.com>
2024-10-01 22:39:14 -0400

    useLogin hook

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,36 +1,23 @@
 import { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 
-const useSignup = () => {
+const useLogin = () => {
   const [loading, setLoading] = useState(false);
   const { setUser } = useAuth();
 
-  const signup = async ({
-    displayName,
-    username,
-    password,
-    confirmPassword,
-    avatar,
-  }) => {
-
-    setLoading(true);
+  const login = async (username, password) => {
     try {
-      const res = await fetch("http://localhost:3000/api/auth/signup", {
+      const res = await fetch("http://localhost:3000/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          displayName,
-          username,
-          password,
-          confirmPassword,
-          avatar,
-        }),
+        body: JSON.stringify({ username, password }),
       });
 
       const data = await res.json();
       if (data.error) {
         throw new Error(data.error);
       }
+
       localStorage.setItem("user", JSON.stringify(data));
       setUser(data);
     } catch (error) {
@@ -39,7 +26,8 @@ const useSignup = () => {
       setLoading(false);
     }
   };
-  return { loading, signup };
+
+  return { loading, login };
 };
 
-export default useSignup;
+export default useLogin;
